e8b17f656ecdad6653c840c8f017f3e5ef171f81
RenanGAS <renansakaoki@gmail.com>
2022-04-02 17:47:27 -0300

    feat: add the Calculator's base code structure

---
+++ src/calculator.hpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+enum Digit
+{
+    ZERO,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE
+};
+
+class Display
+{
+public:
+    void add(Digit digit) {}
+    void clear() {}
+};
+
+class Key
+{
+    Keyboard *keyboard;
+    Digit digit;
+
+public:
+    Key(Digit d) : digit(d) {}
+    void press() {
+        this->keyboard->receiveDigit(this->digit);
+    }
+    void setKeyboard(Keyboard *keyboard)
+    {
+        this->keyboard = keyboard;
+    }
+};
+
+class Keyboard
+{
+    Key *keys[200];
+    int KeysCount;
+    Cpu *cpu;
+
+public:
+    void addKey(Key *key)
+    {
+        this->keys[this->KeysCount++] = key;
+        key->setKeyboard(this);
+    }
+
+    void receiveDigit(Digit d){}
+};
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "calculator.h"
+
+int main()
+{
+    Keyboard k1;
+    Key key0(ZERO), key1(ONE);
+
+    k1.addKey(key0);
+    k1.addKey(key1);
+}
