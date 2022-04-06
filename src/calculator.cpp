d8bf231452505ec9eaca0c0801e887e6defbd217
GUILHERME AUGUSTO RODRIGUES MATURANA <a2349353@debian.dacom>
2022-04-06 15:25:51 -0300

    Put the implmentations on .cpp

---
+++ src/calculator.cpp
@@ -0,0 +1,229 @@
+#include "calculator.hpp"
+
+
+void Display::add(Digit digit)
+{
+    std::cout << int(digit);
+}
+void Display::add(Operation op) {}
+void Display::add(Control command) {}
+void Display::show(Cpu *cpu)
+{
+    Digit *number1 = (cpu->getOperand1());
+    Digit *number2 = (cpu->getOperand2());
+    Operation op = cpu->getOperation();
+}
+void Display::clear() {}
+
+void Key::setKeyboard(Keyboard *keyboard)
+{
+    this->keyboard = keyboard;
+}
+
+Control_key::Control_key(Control comm)
+{
+    this->command = comm;
+}
+
+void Control_key::press()
+{
+    this->keyboard->receiveKeyPress(this->command);
+}
+
+Digit_key::Digit_key(Digit d)
+{
+    this->digit = d;
+}
+
+void Digit_key::press()
+{
+    this->keyboard->receiveKeyPress(this->digit);
+}
+
+Operation_Key::Operation_Key(Operation op)
+{
+    this->op = op;
+}
+
+void Operation_Key::press()
+{
+    this->keyboard->receiveKeyPress(this->op);
+}
+
+Keyboard::Keyboard(Cpu *cpu)
+{
+    this->cpu = cpu;
+    this->KeysCount = 0;
+}
+
+void Keyboard::addKey(Key *key)
+{
+    this->keys[this->KeysCount++] = key;
+    key->setKeyboard(this);
+}
+void Keyboard::receiveKeyPress(Digit digit)
+{
+    this->cpu->receiveInput(digit);
+}
+void Keyboard::receiveKeyPress(Operation op)
+{
+    this->cpu->receiveInput(op);
+}
+void Keyboard::receiveKeyPress(Control command)
+{
+    this->cpu->receiveInput(command);
+}
+void Cpu::left_align(int arg)
+{
+        int helper; //number of digits in the number
+        if(arg == 2)
+        {
+            helper = this->count2; 
+            for(int i = 0; i < helper; i++) //transfers the numbers to the rightmost side
+            {
+                arg2[MAX_DIGITS - helper + i] = arg2[i];
+                arg2[i] = ZERO;
+            }
+            this->count2 = MAX_DIGITS;
+        }
+        else
+        {
+            helper = this->count1;
+            for(int i = 0; i < helper; i++) //transfers the numbers to the rightmost side
+            {
+                arg1[MAX_DIGITS - helper + i] = arg1[i];
+                arg1[i] = ZERO;
+            }
+            this->count1 = MAX_DIGITS;
+        }
+}
+void Cpu::clear_array(Digit *array)
+{
+    for(int i = 0; i < MAX_DIGITS; i++)
+    {
+        array[i] = ZERO;
+    }
+}
+
+int Cpu::convert_to_int(Digit *arg, int count)
+{
+    int result = 0;
+    for(int i = 0; i < count; i++)
+    {
+        result += arg[i] * pow(10, count - i - 1);
+    }
+    return result;
+}
+
+void Cpu::convert_to_digit(int arg, Digit *result)
+{
+    int helper = arg;
+    int i = 0;
+    while(helper != 0)
+    {
+        result[i++] = Digit(helper % 10);
+        helper /= 10;
+    }
+    this->count1 = i;
+}
+
+Cpu::Cpu(Display *display)
+{
+    this->display = display;
+    this->arg1 = static_cast <Digit*> (calloc(MAX_DIGITS, sizeof(Digit)));
+    this->arg2 = static_cast <Digit*> (calloc(MAX_DIGITS, sizeof(Digit)));
+    this->count1 = 0;
+    this->count2 = 0;
+    this->op = NONE;
+}
+
+void Cpu::receiveInput(Digit d)
+{
+    if ((count1 < MAX_DIGITS))
+    {
+        arg1[count1++] = d;
+    }
+    else if ((count2 < MAX_DIGITS) && (this->op != NONE))
+    {
+        arg2[count2++] = d;
+    }
+    this->display->show(this);
+}
+
+void Cpu::receiveInput(Operation op)
+{
+    if(this->op != NONE)
+    {
+        if(this->count2 != 0)
+        {
+            left_align(2);
+            this->operate();
+        }
+    }
+    else
+    {
+        left_align(1);
+    }
+    this->op = op;
+
+    this->display->show(this);
+}
+
+void Cpu::receiveInput(Control command)
+{
+    //TODO: implement
+}
+
+void Cpu::operate()
+{
+    int operand1 = this->convert_to_int(this->arg1, this->count1);
+        int operand2 = this->convert_to_int(this->arg2, this->count2);
+        int result;
+        switch (this->op)
+        {
+            case PLUS:
+                result = operand1 + operand2;
+                //TODO: check if the result is bigger than MAX_DIGITS
+                break;
+            case MINUS:
+                result = operand1 - operand2;
+                break;
+            case TIMES:
+                result = operand1 * operand2;
+                //TODO: check if the result is bigger than MAX_DIGITS
+                break;
+                case DIVIDED:
+                result = operand1 / operand2;
+                //TODO: check if operand2 is 0
+                break;
+                case SQTR:
+                //TODO: check if operand1 is negative
+                result = sqrt(operand1);
+                break;
+                case NONE:
+                //TODO: this is a mistake and should be handled
+                break;
+                case PERCENT:
+                //TODO: check if it follows a real calculator
+                result = operand1 * 100;
+                break;
+        }
+        convert_to_digit(result, this->arg1);
+        clear_array(this->arg2);
+        this->count2 = 0;
+        this->op = NONE;
+        this->display->show(this);
+}
+
+Digit *Cpu::getOperand1()
+{
+    return this->arg1;
+}
+Digit *Cpu::getOperand2()
+{
+    return this->arg2;
+}
+Operation Cpu::getOperation()
+{
+    return this->op;
+}
--- src/calculator.hpp
+++ src/calculator.hpp
@@ -1,3 +1,6 @@
+#ifndef CALCULATOR.H
+#define CALCULATOR.H
+
 #include <iostream>
 #include <cmath>
 #define MAX_DIGITS 8
@@ -43,18 +46,10 @@ enum Control: unsigned char
 class Display
 {
 public:
-    void add(Digit digit) 
-    {
-        std::cout << int(digit);
-    }
-    void add(Operation op) {}
-    void add(Control command) {}
-    void show(Cpu *cpu)
-    {
-        Digit *number1 = (cpu->getOperand1());
-        Digit *number2 = (cpu->getOperand2());
-        Operation op = cpu->getOperation();
-    }
+    void add(Digit digit);
+    void add(Operation op);
+    void add(Control command);
+    void show(Cpu *cpu);
     void clear() {}
 };
 
@@ -70,21 +65,15 @@ public:
         this->keyboard->receivereceiveDigit(this->digit);
     } */
     virtual void press() = 0;
-    void setKeyboard(Keyboard *keyboard)
-    {
-        this->keyboard = keyboard;
-    }
+    void setKeyboard(Keyboard *keyboard);
 };
 
 class Control_key: public Key
 {
     Control command;
     public:
-    Control_key(Control comm) : command(comm) {}
-    void press()
-    {
-        this->keyboard->receiveKeyPress(this->command);
-    }
+    Control_key(Control comm);
+    void press();
 
     
 };
@@ -93,22 +82,16 @@ class Digit_key: public Key
 {
     Digit digit;
     public:
-    Digit_key(Digit d) : digit(d) {}
-    void press()
-    {
-        this->keyboard->receiveKeyPress(this->digit);
-    }
+    Digit_key(Digit d);
+    void press();
 };
 
 class Operation_Key: public Key
 {
     Operation op;
     public:
-    Operation_Key(Operation op) : op(op) {}
-    void press()
-    {
-        this->keyboard->receiveKeyPress(this->op);
-    }
+    Operation_Key(Operation op);
+    void press();
 };
 
 class Keyboard
@@ -119,188 +102,41 @@ class Keyboard
 
 public:
 
-    Keyboard(Cpu *cpu)
-    {
-        this->cpu = cpu;
-        this->KeysCount = 0;
-    }
+    Keyboard(Cpu *cpu);
 
-    void addKey(Key *key)
-    {
-        this->keys[this->KeysCount++] = key;
-        key->setKeyboard(this);
-    }
-    void receiveKeyPress(Digit d) {}
-    void receiveKeyPress(Operation op){}
-    void receiveKeyPress(Control command){}
+    void addKey(Key *key);
+    void receiveKeyPress(Digit d);
+    void receiveKeyPress(Operation op);
+    void receiveKeyPress(Control command);
 };
 
 class Cpu
 {
     Display *display;
     Digit *arg1;
-    int count1 = 0;
+    int count1;
     Operation op;
     Digit *arg2;
-    int count2 = 0;
+    int count2;
 
-    void left_align(int arg)
-    {
-        int helper; //number of digits in the number
-        if(arg == 2)
-        {
-            helper = this->count2; 
-            for(int i = 0; i < helper; i++) //transfers the numbers to the rightmost side
-            {
-                arg2[MAX_DIGITS - helper + i] = arg2[i];
-                arg2[i] = ZERO;
-            }
-            this->count2 = MAX_DIGITS;
-        }
-        else
-        {
-            helper = this->count1;
-            for(int i = 0; i < helper; i++) //transfers the numbers to the rightmost side
-            {
-                arg1[MAX_DIGITS - helper + i] = arg1[i];
-                arg1[i] = ZERO;
-            }
-            this->count1 = MAX_DIGITS;
-        }
-
-    }
+    void left_align(int arg);
+    void clear_array(Digit *array);
 
 
-    void clear_array(Digit *array)
-    {
-        for(int i = 0; i < MAX_DIGITS; i++)
-        {
-            array[i] = ZERO;
-        }
-    }
+    int convert_to_int(Digit *arg, int count);
 
 
-    int convert_to_int(Digit *arg, int count)
-    {
-        int result = 0;
-        for(int i = 0; i < this->count1; i++)
-        {
-            result += arg[i] * pow(10, count - i - 1);
-        }
-        return result;
-    }
-
-
-    void convert_to_digit(int arg, Digit *result)
-    {
-        int helper = arg;
-        int i = 0;
-        while(helper != 0)
-        {
-            result[i++] = Digit(helper % 10);
-            helper /= 10;
-        }
-        this->count1 = i;
-    }
+    void convert_to_digit(int arg, Digit *result);
 
 
     public:
-    Cpu(Display *display)
-    {
-        this->display = display;
-        this->arg1 = static_cast <Digit*> (calloc(MAX_DIGITS, sizeof(Digit)));
-        this->arg2 = static_cast <Digit*> (calloc(MAX_DIGITS, sizeof(Digit)));
-        this->count1 = 0;
-        this->count2 = 0;
-        this->op = NONE;
-    }
-    void receiveInput(Digit d)
-    {
-        if ((count1 < MAX_DIGITS))
-        {
-            arg1[count1++] = d;
-        }
-        else if ((count2 < MAX_DIGITS) && (this->op != NONE))
-        {
-            arg2[count2++] = d;
-        }
-        this->display->show(this);
-
-
-    }
-    void receiveInput(Operation op)
-    {
-        if(this->op != NONE)
-        {
-            if(this->count2 != 0)
-            {
-                left_align(2);
-                this->operate();
-            }
-            
-        }
-        else
-        {
-            left_align(1);
-        }
-        this->op = op;
-
-        this->display->show(this);
-    }
-    void receiveInput(Control command)
-    {
-    
-    }
-    void operate()
-    {
-        int operand1 = this->convert_to_int(this->arg1, this->count1);
-        int operand2 = this->convert_to_int(this->arg2, this->count2);
-        int result;
-        switch (this->op)
-        {
-            case PLUS:
-                result = operand1 + operand2;
-                //TODO: check if the result is bigger than MAX_DIGITS
-                break;
-            case MINUS:
-                result = operand1 - operand2;
-                break;
-            case TIMES:
-                result = operand1 * operand2;
-                //TODO: check if the result is bigger than MAX_DIGITS
-                break;
-                case DIVIDED:
-                result = operand1 / operand2;
-                //TODO: check if operand2 is 0
-                break;
-                case SQTR:
-                //TODO: check if operand1 is negative
-                result = sqrt(operand1);
-                break;
-                case NONE:
-                //TODO: this is a mistake and should be handled
-                break;
-                case PERCENT:
-                //TODO: check if it follows a real calculator
-                result = operand1 * 100;
-                break;
-        }
-        convert_to_digit(result, this->arg1);
-        clear_array(this->arg2);
-        this->count2 = 0;
-        this->op = NONE;
-        this->display->show(this);
-    }
-    Digit *getOperand1()
-    {
-        return this->arg1;
-    }
-    Digit *getOperand2()
-    {
-        return this->arg2;
-    }
-    Operation getOperation()
-    {
-        return this->op;
-    }
+    Cpu(Display *display);
+    void receiveInput(Digit d);
+    void receiveInput(Operation op);
+    void receiveInput(Control command);
+    void operate();
+    Digit *getOperand1();
+    Digit *getOperand2();
+    Operation getOperation();
 };
+#endif
