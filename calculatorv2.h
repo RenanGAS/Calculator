4d1dc4e289bc830bdc81a67b1efa3d12ca7b5fea
Fgarm <guilhermear.maturana@outlook.com>
2022-04-16 20:11:32 -0300

    Feat: Add all the remaining methods to all classes

--- calculatorv2.h
+++ calculatorv2.h
@@ -12,104 +12,105 @@ enum Signal {POSITIVE, NEGATIVE};
 
 class Display
 {
-  public:
-    void add(Digit );
-    // void add(Operation ); não é mostrada na calculadora elgin
-    virtual void SetDecimalSeparator() = 0;
-    virtual void setSignal(Signal ) = 0;
-    virtual void setError() = 0;
-    void clear();
+	public:
+		void add(Digit );
+		// void add(Operation ); não é mostrada na calculadora elgin
+		virtual void SetDecimalSeparator() = 0;
+		virtual void setSignal(Signal ) = 0;
+		virtual void setError() = 0;
+		void clear();
 };
 
 class Receiver
 {
-  public:
-    virtual void receiveDigit(Digit ) = 0;
-    virtual void receiveOperation(Operation ) = 0;
-    virtual void receiveControl(Control ) = 0;
+	public:
+		virtual void receiveDigit(Digit ) = 0;
+		virtual void receiveOperation(Operation ) = 0;
+		virtual void receiveControl(Control ) = 0;
 };
 
 class Cpu: public Receiver
 {
-    Display *display;
-    Digit *arg1;
-    int count1;
-    Operation op;
-    Digit *arg2;
-    int count2;
+		Display *display;
+		Digit *arg1;
+		int count1;
+		Operation op;
+		Digit *arg2;
+		int count2;
 
-    void left_align(int );
-    void clear_array(Digit * );
-    int convert_to_int(Digit * , int );
-    int convert_to_digit(int , Digit * , int&);
-    void call_display();
-    void error_handle();
-    void Operate();
-  public:
-    Cpu();
-    ~Cpu();
-    void setDisplay(Display* );
-    void receiveDigit(Digit );
-    void receiveOperation(Operation );
-    void receiveControl(Control );
+		void left_align(int );
+		void clear_array(Digit * );
+		int convert_to_int(Digit * , int );
+		int convert_to_digit(int , Digit * , int&);
+		void call_display();
+		void error_handle();
+		void Operate();
+	public:
+		Cpu();
+		~Cpu();
+		void setDisplay(Display* );
+		void receiveDigit(Digit );
+		void receiveOperation(Operation );
+		void receiveControl(Control );
 };
 
 class Key;
 
 class Keyboard: public Receiver
 {
-   Key* keys[200];
-   int KeysCount;
-   Cpu* cpu;
-   public:
-      void setCpu(Cpu );
-      void addKey(Key* );
-
-      void receiveDigit(Digit );
-      void receiveOperation(Operation );
-      void receiveControl(Control );
+	Key* keys[200];
+	int KeysCount;
+	Cpu* cpu;
+	public:
+		void setCpu(Cpu* );
+		void addKey(Key* );
+		void receiveDigit(Digit );
+		void receiveOperation(Operation );
+		void receiveControl(Control );
 
 };
 
 class Calculator{
-  public:
-    virtual void setDisplay(Display* ) = 0;
-    virtual void setCpu(Cpu* ) = 0;
-    virtual void setKeyboard(Keyboard* ) = 0;
+	Display* display;
+	Keyboard* keyboard;
+	Cpu* cpu;
+	public:
+		virtual void setDisplay(Display* ) = 0;
+		virtual void setCpu(Cpu* ) = 0;
+		virtual void setKeyboard(Keyboard* ) = 0;
 };
 
 class Key
 {
-   protected:
-     Receiver* receiver;
-   public:
-      virtual void press() = 0;
-
-      void setKeyboard(Keyboard* );
+	 protected:
+		Receiver* receiver;
+	 public:
+		void setReceiver(Receiver* );
+		virtual void press() = 0;
 };
 
 class KeyDigit: public Key
 {
-     Digit digit;
-   public:
-      KeyDigit(Digit );
-      void press();
+	Digit digit;
+	public:
+		KeyDigit(Digit );
+		void press();
 };
 
 class KeyOperation: public Key
 {
-     Operation operation;
-   public:
-      KeyOperation(Operation );
-      void press();
+		Operation operation;
+	 public:
+		KeyOperation(Operation );
+		void press();
 };
 
 class KeyControl: public Key
 {
-     Control control;
-   public:
-      KeyControl(Control );
-      void press();
+		Control control;
+	 public:
+		KeyControl(Control );
+		void press();
 };
 
 
