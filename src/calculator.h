e48ea638b2bdc508576f1bda913cbce29fed8578
RenanGAS <renansakaoki@gmail.com>
2022-04-18 15:28:21 -0300

    feat: add setOperands to alignLeft the operands before the operation's execution

--- src/calculator.h
+++ src/calculator.h
@@ -3,114 +3,155 @@
 
 #define MAX_DIGITS 8
 
-enum Digit {ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE};
-enum Operation {ADDITION, SUBTRACTION, DIVISION, MULTIPLICATION, SQUARE_ROOT, PERCENTAGE, NONE};
-enum Control {CLEAR, RESET, DECIMAL_SEPARATOR, MEMORY_READ, MEMORY_CLEAR, MEMORY_ADDITION, MEMORY_SUBTRACTION, EQUALS};
-enum Signal {POSITIVE, NEGATIVE};
-
+enum Digit
+{
+	ZERO,
+	ONE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE
+};
+enum Operation
+{
+	ADDITION,
+	SUBTRACTION,
+	DIVISION,
+	MULTIPLICATION,
+	SQUARE_ROOT,
+	PERCENTAGE,
+	NONE
+};
+enum Control
+{
+	CLEAR,
+	RESET,
+	DECIMAL_SEPARATOR,
+	MEMORY_READ,
+	MEMORY_CLEAR,
+	MEMORY_ADDITION,
+	MEMORY_SUBTRACTION,
+	EQUALS
+};
+enum Signal
+{
+	POSITIVE,
+	NEGATIVE
+};
 
 class Display
 {
-	public:
-		void add(Digit );
-		void SetDecimalSeparator();
-		void setSignal(Signal );
-		void setError();
-		void clear();
+public:
+	void add(Digit);
+	void SetDecimalSeparator();
+	void setSignal(Signal);
+	void setError();
+	void clear();
 };
 
 class Receiver
 {
-	public:
-		virtual void receiveDigit(Digit ) = 0;
-		virtual void receiveOperation(Operation ) = 0;
-		virtual void receiveControl(Control ) = 0;
+public:
+	virtual void receiveDigit(Digit) = 0;
+	virtual void receiveOperation(Operation) = 0;
+	virtual void receiveControl(Control) = 0;
 };
 
-class Cpu: public Receiver
+class Cpu : public Receiver
 {
-		Display *display;
-		Digit *arg1;
-		int count1;
-		Operation op;
-		Digit *arg2;
-		int count2;
+	Display *display;
+	Digit *arg1;
+	int count1;
+	Operation op;
+	Digit *arg2;
+	int count2;
 
-		void left_align(int );
-		void clear_array(Digit * );
-		int convert_to_int(Digit * , int );
-		int convert_to_digit(int , Digit * , int&);
-		void call_display();
-		void error_handle();
-		void Operate();
-	public:
-		Cpu();
-		~Cpu();
-		void setDisplay(Display* );
-		void receiveDigit(Digit );
-		void receiveOperation(Operation );
-		void receiveControl(Control );
+	void left_align(int);
+	void clear_array(Digit *, int *);
+	int convert_to_int(Digit *, int);
+	int convert_to_digit(int, Digit *, int *);
+	void call_display();
+	void error_handle();
+	void Operate();
+	void setOperands(int, int);
+
+public:
+	Cpu();
+	~Cpu();
+	void setDisplay(Display *);
+	void receiveDigit(Digit);
+	void receiveOperation(Operation);
+	void receiveControl(Control);
 };
 
 class Key;
 
-class Keyboard: public Receiver
+class Keyboard : public Receiver
 {
-	Key* keys[200];
+	Key *keys[200];
 	int KeysCount;
-	Cpu* cpu;
-	public:
-		Keyboard();
-		void setCpu(Cpu* );
-		void addKey(Key* );
-		void receiveDigit(Digit );
-		void receiveOperation(Operation );
-		void receiveControl(Control );
+	Cpu *cpu;
 
+public:
+	Keyboard();
+	void setCpu(Cpu *);
+	void addKey(Key *);
+	void receiveDigit(Digit);
+	void receiveOperation(Operation);
+	void receiveControl(Control);
 };
 
-class Calculator{
-	Display* display;
-	Keyboard* keyboard;
-	Cpu* cpu;
-	public:
-		virtual void setDisplay(Display* ) = 0;
-		virtual void setCpu(Cpu* ) = 0;
-		virtual void setKeyboard(Keyboard* ) = 0;
+class Calculator
+{
+	Display *display;
+	Keyboard *keyboard;
+	Cpu *cpu;
+
+public:
+	virtual void setDisplay(Display *) = 0;
+	virtual void setCpu(Cpu *) = 0;
+	virtual void setKeyboard(Keyboard *) = 0;
 };
 
 class Key
 {
-	 protected:
-		Receiver* receiver;
-	 public:
-		void setReceiver(Receiver* );
-		virtual void press() = 0;
+protected:
+	Receiver *receiver;
+
+public:
+	void setReceiver(Receiver *);
+	virtual void press() = 0;
 };
 
-class KeyDigit: public Key
+class KeyDigit : public Key
 {
 	Digit digit;
-	public:
-		KeyDigit(Digit );
-		void press();
+
+public:
+	KeyDigit(Digit);
+	void press();
 };
 
-class KeyOperation: public Key
+class KeyOperation : public Key
 {
-		Operation operation;
-	 public:
-		KeyOperation(Operation );
-		void press();
+	Operation operation;
+
+public:
+	KeyOperation(Operation);
+	void press();
 };
 
-class KeyControl: public Key
+class KeyControl : public Key
 {
-		Control control;
-	 public:
-		KeyControl(Control );
-		void press();
-};
+	Control control;
 
+public:
+	KeyControl(Control);
+	void press();
+};
 
 #endif
